cca0edca7ff3f032a93ecf089234abca0380691e
Titouan Galopin <galopintitouan@gmail.com>
2013-05-14 23:49:21 +0200

    Create the logger module

---
+++ src/Etu/Module/LoggerBundle/EtuModuleLoggerBundle.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Etu\Module\LoggerBundle;
+
+use Etu\Core\CoreBundle\Framework\Definition\Module;
+
+class EtuModuleLoggerBundle extends Module
+{
+	/**
+	 * @return bool
+	 */
+	public function mustBoot()
+	{
+		return true;
+	}
+
+	/**
+	 * Module is ready to use ?
+	 *
+	 * @return string
+	 */
+	public function isReadyToUse()
+	{
+		return false;
+	}
+
+	/**
+	 * Module identifier (to be required by other modules)
+	 *
+	 * @return string
+	 */
+	public function getIdentifier()
+	{
+		return 'logger';
+	}
+
+	/**
+	 * Module title (describe shortly its aim)
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return 'Logger';
+	}
+
+	/**
+	 * Module author
+	 *
+	 * @return string
+	 */
+	public function getAuthor()
+	{
+		return 'Titouan Galopin';
+	}
+
+	/**
+	 * Module description
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return 'Traque et créer un log de toutes les erreurs inattendues lors des visites utilisateurs';
+	}
+
+	/**
+	 * Define the modules requirements (the other required modules) using their identifiers
+	 *
+	 * @return array
+	 */
+	public function getRequirements()
+	{
+		return array(
+			// Insert your requirements here
+		);
+	}
+}
