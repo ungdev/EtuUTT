1a4a7e80ebc7fc05db1c8e3a99033ac118d15385
Titotix <titotix@free.fr>
2013-05-27 15:35:53 +0200

    UVBundle init

--- src/Etu/Module/UVBundle/Controller/DefaultController.php
+++ src/Etu/Module/UVBundle/Controller/DefaultController.php
@@ -1,25 +1,78 @@
 <?php
 
+
 namespace Etu\Module\UVBundle\Controller;
 
+
 use Etu\Core\CoreBundle\Framework\Definition\Controller;
+use Etu\Module\UVBundle\Entity\UV;
+
+use Symfony\Component\HttpFoundation\Response;
 
 // Import annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+
+
+
+
+/**
+ * @Route ("/uv")
+ */
+
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route ("/")
      */
 
-
-    public function indexAction($name)
+    public function indexAction()
     {
-        var_dump($name);
+        $uv = new UV('nf05', 'initiation au C', '51', '42', '69', '0', true, false, 6, 'UV SUBLIME', 'Honnêtement, rien.' );
 
-        return array('name' => $name);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($uv);
+        $em->flush();
+
+        echo 'salut rien';
+        /*if($codeUV == 'NF05' || $codeUV == 'nf05') {
+            echo 'NF05 powa';è
+
+        }*/
+
+        return new Response('');
     }
+
+    /**
+     * @Route ("/{codeUV}")
+     * @Template()
+     *
+     */
+
+    public function createAction($codeUV)
+    {
+
+        echo 'salut' . $codeUV;
+        /*
+        $product = new UV();
+        $product->setCm($cm) ;
+        $product->setTd($td) ;
+        $product->setTp($tp) ;
+        $product->setThe($the) ;
+        $product->setAutomne($automne) ;
+        $product->setPrintemps($printemps) ;
+        $product->setCredits($credits) ;
+        $product->setObjectifs($objectifs) ;
+        $product->setProgramme($programme) ;
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+*/
+        return new Response('');
+    }
+
+
+
 }
