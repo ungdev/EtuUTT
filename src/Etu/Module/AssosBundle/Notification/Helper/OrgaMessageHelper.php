566a89d1e12d96a980fa516afe84944258419f25
Titouan Galopin <galopintitouan@gmail.com>
2013-06-02 23:11:00 +0200

    Create Organization subscriptions

---
+++ src/Etu/Module/AssosBundle/Notification/Helper/OrgaMessageHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Etu\Module\AssosBundle\Notification\Helper;
+
+use Etu\Core\CoreBundle\Entity\Notification;
+use Etu\Core\CoreBundle\Notification\Helper\HelperInterface;
+
+/**
+ * Notification for a message by an organisation
+ */
+class OrgaMessageHelper implements HelperInterface
+{
+	/**
+	 * @var \Twig_Environment
+	 */
+	protected $twig;
+
+	/**
+	 * @param \Twig_Environment $twig
+	 */
+	public function __construct(\Twig_Environment $twig)
+	{
+		$this->twig = $twig;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'orga_message';
+	}
+
+	/**
+	 * @param Notification $notification
+	 * @return string
+	 */
+	public function render(Notification $notification)
+	{
+		return $this->twig->render('EtuModuleAssosBundle:Notification:orgaMessage.html.twig', array(
+			'notif' => $notification
+		));
+	}
+}
