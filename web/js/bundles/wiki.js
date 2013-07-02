0759388d8faa1874dc94b799607072b77cf4b5bb
Titouan Galopin <galopintitouan@gmail.com>
2013-07-03 00:28:56 +0200

    Bug fix #24

--- web/js/bundles/wiki.js
+++ web/js/bundles/wiki.js
@@ -1,4 +1,19 @@
 
+
+var currentCategories = $('.wiki-nested-item-category-current');
+
+$(function() {
+	currentCategories
+		.removeClass('wiki-nested-item-category-up').addClass('wiki-nested-item-category-down');
+
+	currentCategories
+		.find('.wiki-nested-list-depth-0, .wiki-nested-list-depth-1, .wiki-nested-list-depth-2, .wiki-nested-list-depth-3')
+		.show();
+
+	currentCategories
+		.find('.wiki-nested-link-category').attr('data-state', 'down');
+});
+
 $('.wiki-nested-link-category').click(function() {
 	if ($(this).attr('data-state') == 'up') {
 		$(this).parent().removeClass('wiki-nested-item-category-up').addClass('wiki-nested-item-category-down');
