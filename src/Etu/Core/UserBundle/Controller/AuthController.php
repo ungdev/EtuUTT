df79750cc08a77c630fdac771917b305692df652
Titouan Galopin <galopintitouan@gmail.com>
2013-07-10 00:04:33 +0200

    Bug fix #17

--- src/Etu/Core/UserBundle/Controller/AuthController.php
+++ src/Etu/Core/UserBundle/Controller/AuthController.php
@@ -273,6 +273,14 @@ class AuthController extends Controller
 			'message' => 'user.auth.disconnect.confirm'
 		));
 
+		if (ini_get('session.use_cookies')) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params['path'], $params['domain'],
+				$params['secure'], $params['httponly']
+			);
+		}
+
 		$this->initializeCAS();
 		\phpCAS::setNoCasServerValidation();
 		// \phpCAS::logoutWithRedirectService('http://'.$this->container->getParameter('etu.domain'));
