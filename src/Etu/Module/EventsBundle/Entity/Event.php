8d175cf58def259fa66e804fe725c103214ed066
Titouan Galopin <galopintitouan@gmail.com>
2013-07-16 20:02:56 +0200

    Bug fix about events

--- src/Etu/Module/EventsBundle/Entity/Event.php
+++ src/Etu/Module/EventsBundle/Entity/Event.php
@@ -39,7 +39,7 @@ class Event extends AbstractEvent
 	/**
 	 * @var integer
 	 *
-	 * @ORM\Column(type="integer")
+	 * @ORM\Column(name="id", type="integer")
 	 * @ORM\Id
 	 * @ORM\GeneratedValue(strategy="AUTO")
 	 */
