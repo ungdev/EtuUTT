109bbe67d28626d492171a013161551a8cd9e9a0
Titouan Galopin <galopintitouan@gmail.com>
2013-07-23 23:47:14 +0200

    Add UV notifications

---
+++ src/Etu/Module/UVBundle/Notification/Helper/NewCommentHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Etu\Module\UVBundle\Notification\Helper;
+
+use Etu\Core\CoreBundle\Entity\Notification;
+use Etu\Core\CoreBundle\Notification\Helper\HelperInterface;
+
+/**
+ * Notification for a new comment on an UV
+ */
+class NewCommentHelper implements HelperInterface
+{
+	/**
+	 * @var \Twig_Environment
+	 */
+	protected $twig;
+
+	/**
+	 * @param \Twig_Environment $twig
+	 */
+	public function __construct(\Twig_Environment $twig)
+	{
+		$this->twig = $twig;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'uv_new_comment';
+	}
+
+	/**
+	 * @param Notification $notification
+	 * @return string
+	 */
+	public function render(Notification $notification)
+	{
+		return $this->twig->render('EtuModuleUVBundle:Notification:newComment.html.twig', array(
+			'notif' => $notification
+		));
+	}
+}
---
+++ src/Etu/Module/UVBundle/Notification/Helper/NewReviewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Etu\Module\UVBundle\Notification\Helper;
+
+use Etu\Core\CoreBundle\Entity\Notification;
+use Etu\Core\CoreBundle\Notification\Helper\HelperInterface;
+
+/**
+ * Notification for a new review on an UV
+ */
+class NewReviewHelper implements HelperInterface
+{
+	/**
+	 * @var \Twig_Environment
+	 */
+	protected $twig;
+
+	/**
+	 * @param \Twig_Environment $twig
+	 */
+	public function __construct(\Twig_Environment $twig)
+	{
+		$this->twig = $twig;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'uv_new_review';
+	}
+
+	/**
+	 * @param Notification $notification
+	 * @return string
+	 */
+	public function render(Notification $notification)
+	{
+		return $this->twig->render('EtuModuleUVBundle:Notification:newReview.html.twig', array(
+			'notif' => $notification
+		));
+	}
+}
