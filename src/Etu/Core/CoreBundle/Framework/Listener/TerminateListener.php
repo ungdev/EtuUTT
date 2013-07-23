5c81026a47837a99eef5d5c465fc3ceb574a8c9e
Titouan Galopin <galopintitouan@gmail.com>
2013-07-23 23:59:46 +0200

    Minor bug fix in developpment environnement

--- src/Etu/Core/CoreBundle/Framework/Listener/TerminateListener.php
+++ src/Etu/Core/CoreBundle/Framework/Listener/TerminateListener.php
@@ -2,6 +2,7 @@
 
 namespace Etu\Core\CoreBundle\Framework\Listener;
 
+use Etu\Core\CoreBundle\Framework\EtuKernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
@@ -26,6 +27,10 @@ class TerminateListener
 	 */
 	public function onKernelTerminate(PostResponseEvent $event)
 	{
+		if (substr($event->getRequest()->get('_controller'), 0, 3) != 'Etu') {
+			return;
+		}
+
 		$this->session->set('etu.last_url', $event->getRequest()->getRequestUri());
 	}
 }
