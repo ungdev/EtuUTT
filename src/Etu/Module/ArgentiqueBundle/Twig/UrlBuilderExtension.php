5ab24cda6539ebbcde1e915793791fc9e862b267
Titouan Galopin <galopintitouan@gmail.com>
2015-01-15 22:49:20 +0100

    Bugfix

--- src/Etu/Module/ArgentiqueBundle/Twig/UrlBuilderExtension.php
+++ src/Etu/Module/ArgentiqueBundle/Twig/UrlBuilderExtension.php
@@ -49,7 +49,7 @@ class UrlBuilderExtension extends \Twig_Extension
      */
     public function generateCollectionUrl($path)
     {
-        return $this->builder->getWebPath() . 'argentique/d.php/' . $path;
+        return $this->builder->getWebPath() . '/argentique/d.php/' . $path;
     }
 
     /**
