2ead6c105d4d1e5bcb678ede89b6fdd30ee017c6
Titouan Galopin <galopintitouan@gmail.com>
2015-01-19 16:05:59 +0100

    Bugfix in event view

--- web/src/js/bundles/events.js
+++ web/src/js/bundles/events.js
@@ -1,62 +1,6 @@
 
-var subscriber = {
-	button: $('#subscriber_button'),
-	loader: $('#subscriber_loader'),
-	current: $('#subscriber_current'),
-	yes: $('#subscriber_yes'),
-	probably: $('#subscriber_probably'),
-	no: $('#subscriber_no')
-};
-
-var eventId = subscriber.button.attr('data-event-id');
 var calendar = $('#calendar');
 var loader = $('#loader');
-
-subscriber.yes.click(function() {
-	subscriber.current.text($(this).text());
-	subscriber.button.addClass('disabled');
-	subscriber.loader.show();
-
-	subscriber.button.click();
-
-	$.get(Routing.generate('events_answer', {'id': eventId, 'answer': 'yes'}), function() {
-		subscriber.button.removeClass('disabled');
-		subscriber.loader.hide();
-	});
-
-	return false;
-});
-
-subscriber.probably.click(function() {
-	subscriber.current.text($(this).text());
-	subscriber.button.addClass('disabled');
-	subscriber.loader.show();
-
-	subscriber.button.click();
-
-	$.get(Routing.generate('events_answer', {'id': eventId, 'answer': 'probably'}), function() {
-		subscriber.button.removeClass('disabled');
-		subscriber.loader.hide();
-	});
-
-	return false;
-});
-
-subscriber.no.click(function() {
-	subscriber.current.text($(this).text());
-	subscriber.button.addClass('disabled');
-	subscriber.loader.show();
-
-	subscriber.button.click();
-
-	$.get(Routing.generate('events_answer', {'id': eventId, 'answer': 'no'}), function() {
-		subscriber.button.removeClass('disabled');
-		subscriber.loader.hide();
-	});
-
-	return false;
-});
-
 
 var calendarManager = {
 	persist: function(event) {
