157fbb89119d48d7beba5c5f9be87ca787daa4c7
Titouan Galopin <galopintitouan@gmail.com>
2015-01-19 15:17:42 +0100

    Finalize responsive on events

--- web/src/js/bundles/events_public.js
+++ web/src/js/bundles/events_public.js
@@ -40,7 +40,7 @@ $(function() {
 		}
 	};
 
-	if ($(window).width() > 514) {
+	if ($(window).width() > 680) {
 		options.defaultView = 'month';
 
 		options.header = {
@@ -49,7 +49,7 @@ $(function() {
 			right: 'month,agendaWeek,agendaDay'
 		};
 	} else {
-		options.defaultView = 'monthListView';
+		options.defaultView = 'monthList';
 
 		options.header = {
 			left: 'title',
@@ -60,3 +60,48 @@ $(function() {
 
 	calendar.fullCalendar(options);
 });
+
+
+
+
+var FC = $.fullCalendar;
+var MonthView = FC.views.month;
+
+var MonthListView = MonthView.extend({
+	renderHtml: function() {
+		return '' +
+			'<div class="fc-day-grid-container">' +
+				'<div class="fc-month-list"></div>' +
+			'</div>';
+	},
+
+	renderEvents: function(events) {
+		var container = $('.fc-month-list');
+		container.html('');
+
+		if (events.length > 0) {
+			var list = $('<ul>');
+
+			var event, item;
+
+			for (var i in events) {
+				event = events[i];
+
+				item = $('<li><div class="date"><span class="day" /></div><h5><a /></h5></li>');
+
+				item.find('.day').text(event._start.format('DD/MM'));
+				item.find('a').attr('href', event.url).text(event.title);
+
+				list.append(item);
+			}
+
+			container.append(list);
+		} else {
+			var none = $('<p class="fc-month-list-none">Aucun événement prévu pour le moment</p>');
+
+			container.append(none);
+		}
+	}
+});
+
+FC.views.monthList = MonthListView;
