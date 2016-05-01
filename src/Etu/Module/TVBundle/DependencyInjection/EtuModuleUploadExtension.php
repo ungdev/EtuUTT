a9a762e6487350f3f8f6029f4c7717f18b8bcd1f
Aurélien Labate <aurelien-dev@labate.me>
2016-05-01 20:10:08 +0200

    Move assetic.bundle conf to dependency injection file

--- src/Etu/Module/TVBundle/DependencyInjection/EtuModuleUploadExtension.php
+++ src/Etu/Module/TVBundle/DependencyInjection/EtuModuleUploadExtension.php
@@ -14,15 +14,21 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class EtuModuleTVExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(array $configs, ContainerBuilder $container)
+	{
+		$configuration = new Configuration();
+		$config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-    }
+		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yml');
+
+		$aAsseticBundle = $container->getParameter('assetic.bundles');
+		$aAsseticBundle[] = 'EtuModuleTVBundle';
+        print_r($aAsseticBundle);exit;
+		$container->setParameter('assetic.bundles', $aAsseticBundle);
+
+	}
 }
