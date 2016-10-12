7d7f351a8713796e339bf3be10bd636588231c74
Aurélien Labate <aurelien-dev@labate.me>
2016-10-12 11:19:00 +0200

    Add a new super-admin role

--- src/Etu/Core/UserBundle/Command/SetUserAdminCommand.php
+++ src/Etu/Core/UserBundle/Command/SetUserAdminCommand.php
@@ -12,53 +12,53 @@ use Doctrine\ORM\EntityManager;
 
 class SetUserAdminCommand extends ContainerAwareCommand
 {
-	/**
-	 * Configure the command
-	 */
-	protected function configure()
-	{
-		$this
-			->setName('etu:users:set-admin')
-			->setDescription('Promote given user as global admin')
-		;
-	}
+    /**
+     * Configure the command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('etu:users:set-admin')
+            ->setDescription('Promote given user as global super admin')
+        ;
+    }
 
-	/**
-	 * @param \Symfony\Component\Console\Input\InputInterface $input
-	 * @param \Symfony\Component\Console\Output\OutputInterface $output
-	 * @return void
-	 * @throws \RuntimeException
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$dialog = $this->getHelperSet()->get('dialog');
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return void
+     * @throws \RuntimeException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get('dialog');
 
-		$output->writeln('
+        $output->writeln('
 	Welcome to the EtuUTT users grant tool
 
 This command will help you to promote given user as global admin.
 ');
 
-		$user = null;
+        $user = null;
 
-		/** @var EntityManager $em */
-		$em = $this->getContainer()->get('doctrine')->getManager();
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManager();
 
-		while (! $user instanceof User) {
-			$login = $dialog->ask($output, 'User login: ');
+        while (! $user instanceof User) {
+            $login = $dialog->ask($output, 'User login: ');
 
-			$user = $em->getRepository('EtuUserBundle:User')->findOneBy(array('login' => $login));
+            $user = $em->getRepository('EtuUserBundle:User')->findOneBy(array('login' => $login));
 
-			if (! $user) {
-				$output->writeln("The given login can not be found. Please retry.\n");
-			}
-		}
+            if (! $user) {
+                $output->writeln("The given login can not be found. Please retry.\n");
+            }
+        }
 
-		$user->storeRole('ROLE_ADMIN');
+        $user->storeRole('ROLE_SUPERADMIN');
 
-		$em->persist($user);
-		$em->flush();
+        $em->persist($user);
+        $em->flush();
 
-		$output->writeln("The user ".$user->getLogin()." has been promoted as global admin.\n");
-	}
+        $output->writeln("The user ".$user->getLogin()." has been promoted as global admin.\n");
+    }
 }
