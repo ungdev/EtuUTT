a6d9e4174aaacc533c8ae58c496df66a030d0d39
Aurélien Labate <aurelien-dev@labate.me>
2016-10-12 18:21:19 +0200

    Fix unit tests

--- src/Etu/Core/UserBundle/DataFixtures/ORM/LoadUsersData.php
+++ src/Etu/Core/UserBundle/DataFixtures/ORM/LoadUsersData.php
@@ -5,74 +5,73 @@ namespace Etu\Core\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-
 use Etu\Core\UserBundle\Entity\Member;
 use Etu\Core\UserBundle\Entity\Organization;
 use Etu\Core\UserBundle\Entity\User;
 
 class LoadUsersData extends AbstractFixture implements OrderedFixtureInterface
 {
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getOrder()
-	{
-		return 1;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function load(ObjectManager $manager)
-	{
-		$admin = new User();
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $admin = new User();
 
-		$admin->setFullName('Admin ADMIN');
-		$admin->setLogin('admin');
-		$admin->setMail('admin@utt.fr');
-		$admin->storeRole('ROLE_ADMIN');
-		$admin->setIsStudent(true);
-		$admin->setPassword('OwsJMxEhQi83BQ=='); // 'admin' with the default secret key
-		$admin->setAvatar('admin.png');
-		$admin->setBirthday(new \DateTime());
-		$admin->setLastVisitHome(new \DateTime());
-		$admin->setReadOnlyExpirationDate(new \DateTime());
+        $admin->setFullName('Admin ADMIN');
+        $admin->setLogin('admin');
+        $admin->setMail('admin@utt.fr');
+        $admin->storeRole('ROLE_SUPERADMIN');
+        $admin->setIsStudent(true);
+        $admin->setPassword('OwsJMxEhQi83BQ=='); // 'admin' with the default secret key
+        $admin->setAvatar('admin.png');
+        $admin->setBirthday(new \DateTime());
+        $admin->setLastVisitHome(new \DateTime());
+        $admin->setReadOnlyExpirationDate(new \DateTime());
 
-		$user = new User();
+        $user = new User();
 
-		$user->setFullName('User USER');
-		$user->setLogin('user');
-		$user->setIsStudent(true);
-		$user->setPassword('Ox8JJBEpQjQ='); // 'user' with the default secret key
-		$user->setMail('user@utt.fr');
-		$user->setAvatar('user.png');
-		$user->setBirthday(new \DateTime());
-		$user->setLastVisitHome(new \DateTime());
-		$user->setReadOnlyExpirationDate(new \DateTime());
+        $user->setFullName('User USER');
+        $user->setLogin('user');
+        $user->setIsStudent(true);
+        $user->setPassword('Ox8JJBEpQjQ='); // 'user' with the default secret key
+        $user->setMail('user@utt.fr');
+        $user->setAvatar('user.png');
+        $user->setBirthday(new \DateTime());
+        $user->setLastVisitHome(new \DateTime());
+        $user->setReadOnlyExpirationDate(new \DateTime());
 
-		$orga = new Organization();
+        $orga = new Organization();
 
-		$orga->setName('Orga ORGA');
-		$orga->setLogin('orga');
-		$orga->setContactMail('orga@utt.fr');
+        $orga->setName('Orga ORGA');
+        $orga->setLogin('orga');
+        $orga->setContactMail('orga@utt.fr');
 
-		$manager->persist($admin);
-		$manager->persist($user);
-		$manager->persist($orga);
+        $manager->persist($admin);
+        $manager->persist($user);
+        $manager->persist($orga);
 
-		$membership = new Member();
+        $membership = new Member();
 
-		$membership->setOrganization($orga);
-		$membership->setUser($user);
-		$membership->addPermission('daymail');
+        $membership->setOrganization($orga);
+        $membership->setUser($user);
+        $membership->addPermission('daymail');
 
-		$manager->persist($membership);
+        $manager->persist($membership);
 
-		$manager->flush();
+        $manager->flush();
 
-		$this->addReference('user_admin', $admin);
-		$this->addReference('user_user', $user);
-		$this->addReference('user_orga', $orga);
-		$this->addReference('user_membership', $orga);
-	}
+        $this->addReference('user_admin', $admin);
+        $this->addReference('user_user', $user);
+        $this->addReference('user_orga', $orga);
+        $this->addReference('user_membership', $orga);
+    }
 }
