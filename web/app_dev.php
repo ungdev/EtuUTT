191c73fa88d1465f7b0677611bc2f0db5a10d133
Aurélien Labate <aurelien-dev@labate.me>
2016-10-25 03:19:31 +0200

    Authorize local network to access app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,9 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])
+        || preg_match('/(^127\.)|(^10\.)|(^192\.168\.)/', $_SERVER['REMOTE_ADDR'])
+        || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
