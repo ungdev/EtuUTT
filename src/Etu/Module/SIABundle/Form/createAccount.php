baea8c7954c90d4473b470978c11b57f1e0cc5dc
Christian d'Autume <christian@dautume.fr>
2019-03-21 01:19:59 +0100

    EtuSIA: - Création de compte sur le SIA - Reset password

---
+++ src/Etu/Module/SIABundle/Form/createAccount.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Etu\Module\SIABundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+
+class createAccount extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username', TextType::class, [
+            'disabled' => true,
+            'required' => false,
+            'label' => "Nom d'utilisateur",
+        ])
+            ->add('plainPassword', RepeatedType::class, [
+                'required' => true,
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                ],
+                'second_options' => [
+                    'label' => 'Répête ton mot de passe',
+                ],
+            ])
+            ->add('termsAccepted', CheckboxType::class, [
+                'required' => true,
+                'mapped' => false,
+                'constraints' => new IsTrue(),
+                'label' => "J'accepte les conditions d'utilisation",
+            ])
+            ->add('infoTransfert', CheckboxType::class, [
+                'required' => true,
+                'mapped' => false,
+                'constraints' => new IsTrue(),
+                'label' => "J'accepte la transmission de mes informations personnelles nécessaires à la création de mon compte (Nom, Prénom, Adresse mail, numéro étudiant)",
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'etu_module_siabundlecreate_account';
+    }
+}
---
+++ src/Etu/Module/SIABundle/Ldap/LdapManager.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Etu\Module\SIABundle\Ldap;
+
+use Etu\Module\SIABundle\Ldap\Model\User;
+use FreeIPA\APIAccess\Main;
+
+class LdapManager
+{
+    /**
+     * @var resource
+     */
+    protected $ipa;
+    protected $host;
+    protected $certificate;
+    protected $kernel;
+    protected $user;
+    protected $user_password;
+    public $logs;
+
+    /**
+     * @param $host
+     * @param $port
+     *
+     * @throws \RuntimeException
+     */
+    public function __construct($host, $user, $pass, $certificate)
+    {
+        $this->connection = null;
+        $this->entityManager = null;
+        $this->host = $host;
+        $this->certificate = $certificate;
+        $this->logs = [];
+        $this->user = $user;
+        $this->user_password = $pass;
+    }
+
+    /**
+     * @param $uid
+     *
+     * @return string
+     */
+    public function findUidFree($uid)
+    {
+        $this->connect();
+        $infos = $this->connection->user()->find([$uid]);
+
+        if (empty($infos[0]) || !isset($infos[0])) {
+            return $uid;
+        }
+
+        $uid_inuse = [];
+        foreach ($infos as $info)
+        {
+            $uid_inuse[]= $info->uid[0];
+        }
+
+        $i = 0;
+        $freeUid = $uid;
+        while(in_array($freeUid, $uid_inuse))
+        {
+            ++$i;
+            $freeUid = $uid.$i;
+        }
+
+
+        return $freeUid;
+    }
+
+    /**
+     * @param $userId
+     *
+     * @return bool|Model\User
+     */
+    public function getUserByEtuId($userId)
+    {
+        $this->connect();
+        $infos = $user_info = $this->connection->user()->findBy('carlicense', 'etu:'.intval($userId));
+
+        if (empty($infos[0]) || !isset($infos[0])) {
+            return false;
+        }
+
+        return $this->map(get_object_vars($infos[0]));
+    }
+
+    /**
+     * @param $studentId
+     *
+     * @return bool|Model\User
+     */
+    public function getUserByStudentId($studentId)
+    {
+        $this->connect();
+        $infos = $user_info = $this->connection->user()->findBy('employeenumber', intval($studentId));
+
+        if (empty($infos[0]) || !isset($infos[0])) {
+            return false;
+        }
+
+        return $this->map(get_object_vars($infos[0]));
+    }
+
+    /**
+     * @param array $values
+     *
+     * @return User
+     */
+    private function map(array $values)
+    {
+
+        $user = new User();
+        $user->setLogin($values['uid'][0]);
+        $user->setMail($values['mail'][0]);
+        $user->setFirstName($values['givenname'][0]);
+        $user->setLastName($values['sn'][0]);
+
+        if(isset($values['employeenumber'][0]))
+            $user->setStudentId($values['employeenumber'][0]);
+
+        if(isset($values['carlicense'][0]))
+        {
+            $ids = explode(':', $values['carlicense'][0]);
+            $user->setEtuUttId($ids[1]);
+        }
+
+        return $user;
+    }
+
+    private function connect()
+    {
+        if ($this->connection) {
+            return;
+        }
+        if(!file_exists($this->certificate))
+            file_put_contents($this->certificate, fopen('https://'.$this->host.'/ipa/config/ca.crt', 'r'));
+
+        $this->connection = new Main($this->host, $this->certificate);
+        $this->connection->connection()->authenticate($this->user, $this->user_password);
+    }
+
+    private function mapToIpa(User $user)
+    {
+        $data = [
+            'givenname' => $user->getFirstName(),
+            'sn'    => $user->getLastName(),
+            'uid'   => $user->getLogin(),
+            'mail'  => $user->getMail(),
+            'carlicense'    => 'etu:'.$user->getEtuUttId()
+        ];
+        if($user->getStudentId())
+            $data['employeenumber'] = $user->getStudentId();
+        if($user->getUserPassword())
+            $data['userpassword'] = $user->getUserPassword();
+
+        return $data;
+    }
+    public function create(User $user)
+    {
+        $this->connect();
+
+        $data = $this->mapToIpa($user);
+
+        return $this->connection->user()->add($data);
+    }
+
+    public function modify(User $user)
+    {
+        $this->connect();
+
+        $data = $this->mapToIpa($user);
+        $uid = $data['uid'];
+        unset($data['uid']);
+        return $this->connection->user()->modify($uid, $data);
+    }
+}
