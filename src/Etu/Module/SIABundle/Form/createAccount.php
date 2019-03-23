9c03691295628c3b34588b7b6120aa26e7ba0fcf
Jonas DOREL <jonas.dorel@laposte.net>
2019-03-23 17:48:27 +0100

    etusia: conditions transmission info perso

--- src/Etu/Module/SIABundle/Form/createAccount.php
+++ src/Etu/Module/SIABundle/Form/createAccount.php
@@ -40,7 +40,7 @@ class createAccount extends AbstractType
                 'required' => true,
                 'mapped' => false,
                 'constraints' => new IsTrue(),
-                'label' => "J'accepte la transmission de mes informations personnelles nécéssaires à la création de mon compte (nom, prénom, adresse mail, numéro étudiant)",
+                'label' => "J'accepte la transmission de certaines de mes informations personnelles, nécessaires à la création de mon compte, au SIA. Ceci inclut: nom, prénom, adresse mail et numéro étudiant",
             ]);
     }
 
