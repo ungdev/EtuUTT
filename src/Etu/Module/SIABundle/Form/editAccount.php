901a2bc8e79b33be6c707cc5829292662969c322
Christian d'Autume <christian@dautume.fr>
2019-03-25 22:17:05 +0100

    Cleaning + php-freeipa switch

--- src/Etu/Module/SIABundle/Form/editAccount.php
+++ src/Etu/Module/SIABundle/Form/editAccount.php
@@ -3,7 +3,6 @@
 namespace Etu\Module\SIABundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
