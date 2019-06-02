f997044a78daa00f7b7396068e669eac0696e475
Arnaud9145 <arnaud.dufour@utt.fr>
2019-06-02 18:44:03 +0200

    send notification to device. TODO : format title and message based on notification type

--- src/Etu/Core/CoreBundle/Notification/NotificationSender.php
+++ src/Etu/Core/CoreBundle/Notification/NotificationSender.php
@@ -5,6 +5,7 @@ namespace Etu\Core\CoreBundle\Notification;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\ORM\EntityManager;
 use Etu\Core\CoreBundle\Entity\Notification;
+use Solvecrew\ExpoNotificationsBundle\Manager\NotificationManager;
 
 class NotificationSender
 {
@@ -12,13 +13,18 @@ class NotificationSender
      * @var Registry
      */
     protected $doctrine;
+    /**
+     * @var NotificationManager
+     */
+    protected $notification_manager;
 
     /**
      * @param Registry $doctrine
      */
-    public function __construct(Registry $doctrine)
+    public function __construct(Registry $doctrine, NotificationManager $notification_manager)
     {
         $this->doctrine = $doctrine;
+        $this->notification_manager = $notification_manager;
     }
 
     /**
@@ -33,6 +39,8 @@ class NotificationSender
     {
         /** @var $em EntityManager */
         $em = $this->doctrine->getManager();
+
+        $this->sendToMobiles($notif);
 
         if (!$notif->getIsSuper() && $tryCompile) {
             $oldDate = new \DateTime();
@@ -69,5 +77,61 @@ class NotificationSender
         $em->flush();
 
         return true;
+    }
+
+
+    /**
+     * 
+     * Trouve tout les clients natif capable de recevoir des notifications push
+     * Parmi ces devices, ne garde que ceux dont l'utilisateur peur recevoir la notification
+     * Envoie la notification
+     * 
+     * @param Module[] $enabledModules
+     *
+     * @return \Etu\Core\CoreBundle\Entity\Notification[]
+     */
+    public function sendToMobiles($notification)
+    {
+        $em = $this->doctrine->getManager();
+
+        $all_devices = $em->getRepository('EtuCoreApiBundle:OauthClient')->findBy(['native' => 1, 'deletedAt' => null]);
+        $filter = [];
+        foreach($all_devices as $client) { //filter to get only devices with a push token
+          if($client->getPushToken() != null) {
+            array_push($filter, $client);
+          }
+        }
+        $all_devices = $filter;
+
+        /** @var $subscriptions Subscription[] */
+        $subscriptions = $em->getRepository('EtuCoreBundle:Subscription')
+          ->findBy([
+            'entityType' => $notification->getEntityType(),
+            'entityId' => $notification->getEntityId()
+            ]);
+
+        $tokens = [];
+        foreach($subscriptions as $subscription) {
+          $user = $subscription->getUser();
+          foreach($all_devices as $client) {
+            if($client->getUser() == $user) {
+              array_push($tokens, $client->getPushToken());
+            }
+          }
+        }
+
+        $notificationManager = $this->notification_manager;
+        $title = 'New Notification';
+        $message = 'Hello there!';
+        //$token = $body['token'];
+        $data = ['title' => $title, 'message' => $message];
+
+        $notificationManager->sendNotifications(
+            [$message],
+            $tokens,
+            [$title],
+            [$data]
+        );
+       
     }
 }
