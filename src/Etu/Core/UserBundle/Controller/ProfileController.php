c6a756113e545675a5947e1874c5f06fafb25557
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2020-07-24 21:59:56 +0200

    label fix

--- src/Etu/Core/UserBundle/Controller/ProfileController.php
+++ src/Etu/Core/UserBundle/Controller/ProfileController.php
@@ -158,7 +158,8 @@ class ProfileController extends Controller
             'required' => false,
             'label' => 'user.profile.profileEdit.privacy',
         ];
-
+        $privacyChoiceSchedule = $privacyChoice;
+        $privacyChoiceSchedule['label'] = 'user.profile.profile.schedule';
         $form = $this->createFormBuilder($user)
             ->add('phoneNumber', null, ['required' => false, 'label' => 'user.profile.profileEdit.phoneNumber'])
             ->add('phoneNumberPrivacy', ChoiceType::class, $privacyChoice)
@@ -192,7 +193,7 @@ class ProfileController extends Controller
                 ], ])
             ->add('personnalMail', EmailType::class, ['required' => false, 'label' => 'user.profile.profileEdit.personnalMail'])
             ->add('personnalMailPrivacy', ChoiceType::class, $privacyChoice)
-            ->add('schedulePrivacy', ChoiceType::class, $privacyChoice)
+            ->add('schedulePrivacy', ChoiceType::class, $privacyChoiceSchedule)
             ->add('website', null, ['required' => false, 'label' => 'user.profile.profileEdit.website'])
             ->add('facebook', null, ['required' => false, 'label' => 'user.profile.profileEdit.facebook'])
             ->add('twitter', null, ['required' => false, 'label' => 'user.profile.profileEdit.twitter'])
