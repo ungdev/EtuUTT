5a39e05509875412b51e9a2fbdf810365c92909b
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2020-08-23 23:48:12 +0200

    conf from env var fully working

--- web/app.php
+++ web/app.php
@@ -5,10 +5,6 @@ use Symfony\Component\HttpFoundation\Request;
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
-if (file_exists(__DIR__.'/../.env.local')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../', '.env.local');
-    $dotenv->load();
-}
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
--- web/app_dev.php
+++ web/app_dev.php
@@ -7,10 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
 // for more information
 //umask(0000);
-if (file_exists(__DIR__.'/../.env.local')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../', '.env.local');
-    $dotenv->load();
-}
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
