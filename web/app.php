c707d50c26dcff12ce70926e7ab98f049315561e
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2020-08-24 01:25:14 +0200

    php cs fix

--- web/app.php
+++ web/app.php
@@ -6,8 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
-if($_ENV["ETUUTT_APP_ENV"] != "prod")
-{
+if ('prod' != $_ENV['ETUUTT_APP_ENV']) {
     if (isset($_SERVER['HTTP_CLIENT_IP'])
         || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
         || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])
@@ -18,10 +17,8 @@ if($_ENV["ETUUTT_APP_ENV"] != "prod")
         exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
     }
     Debug::enable();
-    $kernel = new AppKernel($_ENV["ETUUTT_APP_ENV"], true);
-}
-else
-{
+    $kernel = new AppKernel($_ENV['ETUUTT_APP_ENV'], true);
+} else {
     $kernel = new AppKernel('prod', false);
 }
 $kernel->loadClassCache();
