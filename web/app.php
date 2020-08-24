08e9c86f97095b8da8c8861c5d16f0a224c8c731
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2020-08-24 20:12:20 +0200

    complete remove security for dev env

--- web/app.php
+++ web/app.php
@@ -7,15 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 if ('prod' != $_ENV['ETUUTT_APP_ENV']) {
-    if (isset($_SERVER['HTTP_CLIENT_IP'])
-        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-        || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])
-            || preg_match('/(^127\.)|(^10\.)|(^192\.168\.)/', $_SERVER['REMOTE_ADDR'])
-            || PHP_SAPI === 'cli-server')
-    ) {
-        header('HTTP/1.0 403 Forbidden');
-        exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-    }
     Debug::enable();
     $kernel = new AppKernel($_ENV['ETUUTT_APP_ENV'], true);
 } else {
